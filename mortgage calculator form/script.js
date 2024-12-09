0b338fd76fcc180fa6480f841dbbb18baa51bd50
mjk22071998 <mjk22071998@gmail.com>
2024-12-09 17:45:28 +0500

    Updated mortgage project

--- mortgage calculator form/script.js
+++ mortgage calculator form/script.js
@@ -1,42 +1,33 @@
-function validateForm() {
-    const mortgageAmount = document.getElementById("amount").value;
-    const mortgageTerm = document.getElementById("time").value;
-    const interestRate = document.getElementById("interest").value;
+const mortgageAmount = document.getElementById("amount");
+const mortgageTerm = document.getElementById("time");
+const interestRate = document.getElementById("interest");
+const monthly = document.getElementById("monthly");
+const yearly = document.getElementById("yearly");
 
-    if (mortgageAmount === '' || mortgageAmount <= 0) {
-        alert('Mortgage Amount must be a positive number.');
-        return false;
-    }
-
-    if (mortgageTerm === '' || mortgageTerm <= 0) {
-        alert('Mortgage Term must be a positive number.');
-        return false;
-    }
-
-    if (interestRate === '' || interestRate < 0) {
-        alert('Interest Rate must be a non-negative number.');
-        return false;
-    }
-
-    return true;
+//Update function to update the mortgate values
+function update() {
+    const monthPayment = Number(mortgageAmount.value) + Number(mortgageAmount.value) * Number(interestRate.value) / 100;
+    const total = monthPayment * 12 * Number(mortgageTerm.value);
+    let formattedMonthlyPayment = new Intl.NumberFormat('en-US', {
+        minimumFractionDigits: 1,
+        maximumFractionDigits: 1,
+    }).format(monthPayment);
+    let formattedTotal = new Intl.NumberFormat('en-US', {
+        minimumFractionDigits: 1,
+        maximumFractionDigits: 1,
+    }).format(total);
+    monthly.innerText = `£${formattedMonthlyPayment}`;
+    yearly.innerText = `£${formattedTotal}`;
 }
 
+// Update on button click
 document.getElementById('calculate').addEventListener('click', function (event) {
-    if (!validateForm()) {
-        event.preventDefault();
-    }
+    event.preventDefault();
+    update();
 });
 
-document.getElementById('calculate').addEventListener('click', function () {
-    console.log("running");
-    var msgDiv = document.getElementById('msg');
-    var calcDiv = document.getElementById('calc');
 
-    if (msgDiv.style.display === 'block') {
-        msgDiv.style.display = 'none';
-        calcDiv.style.display = 'block';
-    } else {
-        msgDiv.style.display = 'block';
-        calcDiv.style.display = 'none';
-    }
-});
+//Adding events to input fields
+mortgageAmount.onkeyup = update;
+mortgageTerm.onkeyup = update;
+interestRate.onkeyup = update;
